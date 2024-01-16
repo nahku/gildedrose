99fa67ca537e6d0c843e5b5423c664362d523096
Ivan Moore <ivan@teamoptimization.com>
2024-01-16 11:13:45 +0000

    adds pitest

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
-./gradlew clean test jacocoTestReport
-open build/jacocoHtml/index.html
+./gradlew clean test jacocoTestReport pitest
+open build/jacocoHtml/index.html && open build/reports/pitest/index.html
